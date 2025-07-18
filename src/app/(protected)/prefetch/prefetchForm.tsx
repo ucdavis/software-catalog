9f33f171521079807b54ac080f392a1f8d535958
Scott Kirkland <srkirkland@gmail.com>
2025-07-18 11:34:56 -0700

    prefetch works except for typing

---
+++ src/app/(protected)/prefetch/prefetchForm.tsx
@@ -0,0 +1,20 @@
+'use client';
+import { useTRPC } from '@/server/trpc/react';
+import { useSuspenseQuery } from '@tanstack/react-query';
+import React from 'react';
+
+export default function PrefetchForm() {
+  // now we are in a client component, so we can use regular react-query to fetch data
+  const trpc = useTRPC();
+  const { data } = useSuspenseQuery(
+    trpc.sample.echo.queryOptions({ message: 'Hello, Prefetch!' })
+  );
+
+  console.log('PrefetchForm data:', data);
+
+  return (
+    <>
+      <p className='text-lg text-gray-600 mb-8'>TBD</p>
+    </>
+  );
+}
