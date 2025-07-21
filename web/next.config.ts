c16eb4df1823afd90a336d8e4eca15717754cdef
Scott Kirkland <srkirkland@gmail.com>
2025-07-21 16:55:52 -0700

    prettier

--- web/next.config.ts
+++ web/next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
-  serverExternalPackages: ["pino", "pino-pretty"],
+  serverExternalPackages: ['pino', 'pino-pretty'],
 };
 
 export default nextConfig;
