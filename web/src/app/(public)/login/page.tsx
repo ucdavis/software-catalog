950c6bbaba440ec44dd38db980fe96b48d853f13
Cal <cydoval@mac.com>
2025-07-22 16:38:02 -0700

    progress

--- web/src/app/(public)/login/page.tsx
+++ web/src/app/(public)/login/page.tsx
@@ -14,12 +14,15 @@ export default async function LoginPage() {
     <div className='min-h-screen flex items-center justify-center bg-gray-50 dark:bg-gray-900 py-12 px-4 sm:px-6 lg:px-8'>
       <div className='max-w-md w-full space-y-8'>
         <div>
-          <h2 className='mt-6 text-center text-3xl font-extrabold text-gray-900 dark:text-white'>
+          <h2 className='mt-6 text-center text-3xl font-extrabold text-gray-900'>
             Sign in to your account
           </h2>
-          <p className='mt-2 text-center text-sm text-gray-600 dark:text-gray-400'>
+          <p className='mt-2 text-center text-sm text-gray-600'>
             Choose your preferred sign-in method
           </p>
+          <p className='text-primary-white'>primary-white</p>
+          <p className='text-ucd-doubledecker'>ucd-doubledeck</p>
+          <p className='text-primary-color'>ucd-doubledeck</p>
         </div>
 
         <div className='mt-8 space-y-4'>
