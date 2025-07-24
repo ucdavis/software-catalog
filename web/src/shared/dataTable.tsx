e9c15d54707b60bfdbda8dfa582db2d8effbcb8d
Scott Kirkland <srkirkland@gmail.com>
2025-07-24 14:26:04 -0700

    daisyUI style

--- web/src/shared/dataTable.tsx
+++ web/src/shared/dataTable.tsx
@@ -35,50 +35,44 @@ export const DataTable = <TData extends object>({
 
   return (
     <div className='space-y-4'>
-      {/* Global filter input */}
-      <div className='flex items-center space-x-2'>
-        <div className='relative flex-1 max-w-sm'>
-          <div className='absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none'>
-            <svg
-              className='h-5 w-5 text-gray-400'
-              xmlns='http://www.w3.org/2000/svg'
-              viewBox='0 0 20 20'
-              fill='currentColor'
-            >
-              <path
-                fillRule='evenodd'
-                d='M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z'
-                clipRule='evenodd'
-              />
-            </svg>
-          </div>
+      <div className='flex items-center justify-between'>
+        <label className='input input-bordered flex items-center gap-2 w-full max-w-sm'>
+          <svg
+            className='h-4 w-4 opacity-70'
+            xmlns='http://www.w3.org/2000/svg'
+            viewBox='0 0 16 16'
+            fill='currentColor'
+          >
+            <path
+              fillRule='evenodd'
+              d='M9.965 11.026a5 5 0 1 1 1.06-1.06l2.755 2.754a.75.75 0 1 1-1.06 1.06l-2.755-2.754ZM10.5 7a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0Z'
+              clipRule='evenodd'
+            />
+          </svg>
           <input
             type='text'
+            className='grow'
             placeholder='Search all columns...'
             value={table.getState().globalFilter ?? ''}
             onChange={(e) => table.setGlobalFilter(e.target.value)}
-            className='input input-bordered w-full pl-10 pr-4 py-2 text-sm'
           />
           {table.getState().globalFilter && (
             <button
               onClick={() => table.setGlobalFilter('')}
-              className='absolute inset-y-0 right-0 pr-3 flex items-center'
+              className='btn btn-ghost btn-sm btn-circle'
+              type='button'
             >
               <svg
-                className='h-4 w-4 text-gray-400 hover:text-gray-600'
+                className='h-4 w-4'
                 xmlns='http://www.w3.org/2000/svg'
-                viewBox='0 0 20 20'
+                viewBox='0 0 16 16'
                 fill='currentColor'
               >
-                <path
-                  fillRule='evenodd'
-                  d='M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z'
-                  clipRule='evenodd'
-                />
+                <path d='M5.28 4.22a.75.75 0 0 0-1.06 1.06L6.94 8l-2.72 2.72a.75.75 0 1 0 1.06 1.06L8 9.06l2.72 2.72a.75.75 0 1 0 1.06-1.06L9.06 8l2.72-2.72a.75.75 0 0 0-1.06-1.06L8 6.94 5.28 4.22Z' />
               </svg>
             </button>
           )}
-        </div>
+        </label>
       </div>
 
       <div className='overflow-x-auto'>
